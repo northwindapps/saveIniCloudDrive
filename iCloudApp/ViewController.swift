9a9f64da0831fb29dc3dceb433db7ba25efb8914
Yujin Yano <northwindapps@gmail.com>
2021-04-05 17:13:36 +0900

    first commit

--- iCloudApp/ViewController.swift
+++ iCloudApp/ViewController.swift
@@ -12,6 +12,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let containerURL = FileManager.default.url(forUbiquityContainerIdentifier: nil)
+        let documentDirectoryURL = containerURL!.appendingPathComponent("Documents")
+        let documentURL = documentDirectoryURL.appendingPathComponent("myFile.txt")
+        let text = String("test message")
+        do{
+            try text.write(to: documentURL, atomically:true, encoding:String.Encoding.utf8)
+        }catch let Error{
+            print(Error)
+        }
     }
 
 
